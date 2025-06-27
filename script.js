4c30cb7c34109538e491b8790fba4d3ed91380a8
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2025-06-28 00:50:04 +0530

    Add smooth scrolling

--- script.js
+++ script.js
@@ -32,3 +32,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Scrolling
+
+const scrollTobtn = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+scrollTobtn.addEventListener('click', function()
+{
+ section1.scrollIntoView({behavior : 'smooth'});
+})
+
